91ac771fdbfc601b747b1b7377ec0f09ee84cc77
Cash Vo (Aquent LLC) <v-ducvo@microsoft.com>
2018-05-02 10:43:40 -0700

    Original code from Lucas...not working yet.

--- app.js
+++ app.js
@@ -1,121 +1,71 @@
-const {BotFrameworkAdapter, FileStorage, ConversationState, UserState, BotStateSet, 
-    MessageFactory, NumberPrompt, TextPrompt, DatetimePrompt } = require("botbuilder");
-const {DialogSet} = require("botbuilder-dialogs");
-const restify = require("restify");
+const { MemoryStorage } = require('botbuilder');
+const botbuilder = require('botbuilder');
+const restify = require('restify');
+const {MessageFactory} = require('botbuilder');
+
+
+const storage = new MemoryStorage();
 
 // Create server
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`${server.name} listening to ${server.url}`);
 });
-// Create adapter
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+
+// Create adapter (it's ok for MICROSOFT_APP_ID and MICROSOFT_APP_PASSWORD to be blank for now)  
+const adapter = new botbuilder.BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
 });
 
-// Storage
-const storage = new FileStorage("c:/temp");
-const convoState = new ConversationState(storage);
-const userState  = new UserState(storage);
-adapter.use(new BotStateSet(convoState, userState));
-
-// Create empty dialog set
-const dialogs = new DialogSet();
 
 // Listen for incoming activity 
 server.post('/api/messages', (req, res) => {
+    // Route received activity to adapter for processing
     adapter.processActivity(req, res, async (context) => {
-        const isMessage = (context.activity.type === 'message');
-        // State will store all of your information 
-        const convo = conversationState.get(context);
-        const dc = dialogs.createContext(context, state);
-
-        if (isMessage) {
-            // Check for valid intents
-            if(context.activity.text.match(/hi/ig)){
-                await dc.begin('greeting');
+        if (context.activity.type === 'message') {
+            const utterances = (context.activity.text || '').trim().toLowerCase()
+            if (utterances === 'subscribe') {
+                const reference = context.activity;
+                const userId = reference.id;
+                const changes = {};
+                changes['reference/' + userId] = reference;
+                await storage.write(changes)
+                await subscribeUser(userId)
+                await context.sendActivity(`Thank You! We will message you shortly.`);
+               
+            } else{
+                await context.sendActivity("Say 'subscribe'");
             }
-            else if(context.activity.text.match(/reserve table/ig)){
-                await dc.begin('reserveTable');
-            }
-        }
-
-        if(!context.responded){
-            // Continue executing the "current" dialog, if any.
-            await dc.continue();
-
-            if(!context.responded && isMessage){
-                // Default message
-                await context.sendActivity("Hi! I'm a simple bot. Please say 'Hi' or 'Reserve table'.");
-            }
+    
         }
     });
 });
 
-// Define dialogs
 
-// Greet user by name:
-// Ask for the user name and then greet them by name.
-dialogs.add('greeting',[
-    async function (dc){
-        dc.currentDialog.state = {};
-        await dc.prompt('textPrompt', 'Hi! What is your name?');
-    },
-    async function(dc, results){
-        dc.currentDialog.state.userName = results;
-        await dc.context.sendActivity(`Hello ${dc.currentDialog.state.userName}!`);
-        await dc.prompt('textPrompt', 'Where do you work?');
-    },
-    async function(dc, results){
-        dc.currentDialog.state.workPlace = results;
-        await dc.context.sendActivity(`${dc.currentDialog.state.workPlace} is a fun place to work.`);
-        await dc.end(); // Ends the dialog
-    }
-]);
+async function subscribeUser(userId) {
+    setTimeout(() => {
+        createContextForUser(userId)
+    }, 2000);
+}
 
-// Define prompts
-// Generic prompts
-dialogs.add('numberPrompt', new NumberPrompt());
-dialogs.add('textPrompt', new TextPrompt());
-dialogs.add('dateTimePrompt', new DatetimePrompt());
-dialogs.add('partySizePrompt', new NumberPrompt());
+async function createContextForUser(userId, callback) {
 
-// Reserve a table:
-// Help the user to reserve a table
-
-dialogs.add('reserveTable', [
-    async function(dc, args, next){
-        await dc.context.sendActivity("Welcome to the reservation service.");
-
-        dc.currentDialog.state.reservationInfo = {}; // Clears any previous data
-        await dc.prompt('dateTimePrompt', "Please provide a reservation date and time.");
-    },
-    async function(dc, result){
-        dc.currentDialog.state.reservationInfo.dateTime = result[0].value;
-
-        // Ask for next info
-        await dc.prompt('partySizePrompt', "How many people are in your party?");
-    },
-    async function(dc, result){
-        dc.currentDialog.state.reservationInfo.partySize = result;
-
-        // Ask for next info
-        await dc.prompt('textPrompt', "Who's name will this be under?");
-    },
-    async function(dc, result){
-        dc.currentDialog.state.reservationInfo.reserveName = result;
+    const reference = await findReference(userId);
+    if (reference) {
+        await adapter.continueConversation(reference, async (context) => {
+           await context.sendActivity("coming back at you");
+        });
         
-        // Persist data
-        var convo = convoState.get(dc.context);; // conversationState.get(dc.context);
-        convo.reservationInfo = dc.currentDialog.state.reservationInfo;
+     }
+    // await callback(adapter.createContext(reference))
+          
+}
 
-        // Confirm reservation
-        var msg = `Reservation confirmed. Reservation details: 
-            <br/>Date/Time: ${dc.currentDialog.state.reservationInfo.dateTime} 
-            <br/>Party size: ${dc.currentDialog.state.reservationInfo.partySize} 
-            <br/>Reservation name: ${dc.currentDialog.state.reservationInfo.reserveName}`;
-        await dc.context.sendActivity(msg);
-        await dc.end();
-    }
-]);
+async function findReference(userId){
+    const referenceKey = 'reference/' + userId;
+    var rows = await storage.read([referenceKey])
+    var reference = await rows[referenceKey]
+    return reference;
+}
+
