ad10ef522a3e6680e44d423b0a8f88df3ac9a822
Cash Vo (Aquent LLC) <v-ducvo@microsoft.com>
2018-05-11 09:37:22 -0700

    Add two numbers dialog

--- app.js
+++ app.js
@@ -29,97 +29,61 @@ const dialogs = new DialogSet();
 
 // Listen for incoming activity 
 server.post('/api/messages', (req, res) => {
+    // Route received activity to adapter for processing
     adapter.processActivity(req, res, async (context) => {
-        const isMessage = (context.activity.type === 'message');
-        // State will store all of your information 
-        const convo = conversationState.get(context);
-        const dc = dialogs.createContext(context, convo);
-
+        const isMessage = context.activity.type === 'message';
         if (isMessage) {
-            // Check for valid intents
-            if(context.activity.text.match(/hi/ig)){
-                await dc.begin('greeting');
+            const state = conversationState.get(context);
+            const count = state.count === undefined ? state.count = 0 : ++state.count;
+
+            // create a dialog context
+            const dc = dialogs.createContext(context, state);
+
+            // MatchesAdd2Numbers checks if the message matches a regular expression
+            // and if it does, returns an array of the numbers to add
+            var numbers = await MatchesAdd2Numbers(context.activity.text); 
+            if (numbers != null && numbers.length >=2 )
+            {    
+                await dc.begin('addTwoNumbers', numbers);
             }
-            else if(context.activity.text.match(/reserve table/ig)){
-                await dc.begin('reserveTable');
-            }
+            else {
+                // Just echo back the user's message if they're not adding numbers
+                return context.sendActivity(`Turn ${count}: You said "${context.activity.text}"`); 
+            }           
+        } else {
+            return context.sendActivity(`[${context.activity.type} event detected]`);
         }
-
-        if(!context.responded){
-            // Continue executing the "current" dialog, if any.
+        if (!context.responded) {
             await dc.continue();
-
-            if(!context.responded && isMessage){
-                // Default message
-                await context.sendActivity("Hi! I'm a simple bot. Please say 'Hi' or 'Reserve table'.");
+            // if the dialog didn't send a response
+            if (!context.responded && isMessage) {
+                await dc.context.sendActivity(`Hi! I'm the add 2 numbers bot. Say something like "what's 1+2?"`);
             }
         }
     });
 });
 
+
 // Define dialogs
 
-// Greet user by name:
-// Ask for the user name and then greet them by name.
-dialogs.add('greeting',[
-    async function (dc){
-        dc.activeDialog.state = {};
-        await dc.prompt('textPrompt', 'Hi! What is your name?');
-    },
-    async function(dc, results){
-        dc.activeDialog.state.userName = results;
-        await dc.context.sendActivity(`Hi ${dc.activeDialog.state.userName}!`);
-        await dc.prompt('textPrompt', 'Where do you work?');
-    },
-    async function(dc, results){
-        dc.activeDialog.state.workPlace = results;
-        await dc.context.sendActivity(`${dc.activeDialog.state.workPlace} is a fun place.`);
-        await dc.end(); // Ends the dialog
-    }
-]);
-
-// Define prompts
-// Generic prompts
-dialogs.add('numberPrompt', new NumberPrompt());
-dialogs.add('textPrompt', new TextPrompt());
-dialogs.add('dateTimePrompt', new DatetimePrompt());
-dialogs.add('partySizePrompt', new NumberPrompt());
-
-// Reserve a table:
-// Help the user to reserve a table
-
-dialogs.add('reserveTable', [
-    async function(dc, args, next){
-        await dc.context.sendActivity("Welcome to the reservation service.");
-
-        dc.activeDialog.state.reservationInfo = {}; // Clears any previous data
-        await dc.prompt('dateTimePrompt', "Please provide a reservation date and time.");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.dateTime = result[0].value;
-
-        // Ask for next info
-        await dc.prompt('partySizePrompt', "How many people are in your party?");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.partySize = result;
-
-        // Ask for next info
-        await dc.prompt('textPrompt', "Who's name will this be under?");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.reserveName = result;
-        
-        // Persist data
-        var convo = conversationState.get(dc.context);; // conversationState.get(dc.context);
-        convo.reservationInfo = dc.activeDialog.state.reservationInfo;
-
-        // Confirm reservation
-        var msg = `Reservation confirmed. Reservation details: 
-            <br/>Date/Time: ${dc.activeDialog.state.reservationInfo.dateTime} 
-            <br/>Party size: ${dc.activeDialog.state.reservationInfo.partySize} 
-            <br/>Reservation name: ${dc.activeDialog.state.reservationInfo.reserveName}`;
-        await dc.context.sendActivity(msg);
+// Show the sum of two numbers.
+dialogs.add('addTwoNumbers', [async function (dc, numbers){
+        var sum = Number.parseFloat(numbers[0]) + Number.parseFloat(numbers[1]);
+        await dc.context.sendActivity(`${numbers[0]} + ${numbers[1]} = ${sum}`);
         await dc.end();
+    }]
+);
+
+async function MatchesAdd2Numbers(message) {
+    const ADD_NUMBERS_REGEXP = /([-+]?(?:[0-9]+(?:\.[0-9]+)?|\.[0-9]+))(?:\s*)\+(?:\s*)([-+]?(?:[0-9]+(?:\.[0-9]+)?|\.[0-9]+))/i;
+    let matched = ADD_NUMBERS_REGEXP.exec(message);
+    if (!matched) {
+        // message wasn't a request to add 2 numbers
+        return null;
     }
-]);
+    else {
+        var numbers = [matched[1], matched[2]];
+        return numbers;
+    }
+}
+
