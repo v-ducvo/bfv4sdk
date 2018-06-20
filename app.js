7540db1fdfe1a523f5b0c3f2f358670472e31f34
Cash Vo (Aquent LLC) <v-ducvo@microsoft.com>
2018-06-19 17:16:20 -0700

    working sample

--- app.js
+++ app.js
@@ -1,128 +1,77 @@
-// Bot Framework v4 SDK Sample
-// This bot will greet the user by name and help user reserve a table
-//
+const { BotFrameworkAdapter, MemoryStorage, ConversationState } = require('botbuilder');
+const restify = require('restify');
+const {DialogSet, TextPrompt, NumberPrompt, ChoicePrompt} = require("botbuilder-dialogs");
+const dialogs = new DialogSet();
+const { recognizeChoices } = require('botbuilder-choices');
 
-const {BotFrameworkAdapter, FileStorage, ConversationState, UserState, BotStateSet, 
-    MessageFactory } = require("botbuilder");
-const {NumberPrompt, TextPrompt, DatetimePrompt, DialogSet} = require("botbuilder-dialogs");
-const restify = require("restify");
 
 // Create server
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`${server.name} listening to ${server.url}`);
 });
+
 // Create adapter
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+const adapter = new BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
 });
 
-// Storage
-const storage = new FileStorage("c:/temp");
-const conversationState = new ConversationState(storage);
-const userState  = new UserState(storage);
-adapter.use(new BotStateSet(conversationState, userState));
+// Add conversation state middleware
+const conversationState = new ConversationState(new MemoryStorage());
+adapter.use(conversationState);
 
-// Create empty dialog set
-const dialogs = new DialogSet();
-
-// Listen for incoming activity 
+// Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
+    // Route received request to adapter for processing
     adapter.processActivity(req, res, async (context) => {
-        const isMessage = (context.activity.type === 'message');
-        // State will store all of your information 
-        const convo = conversationState.get(context);
-        const dc = dialogs.createContext(context, convo);
+        if (context.activity.type === 'message') {
+            const state = conversationState.get(context);
+            const count = state.count === undefined ? state.count = 0 : ++state.count;
+            const dc = dialogs.createContext(context, state);
 
-        if (isMessage) {
-            // Check for valid intents
-            if(context.activity.text.match(/hi/ig)){
-                var workplace = await dc.begin('greeting');
-                if(workplace){
-                    console.log("Workplace: " + workplace);
-                }
-            }
-            else if(context.activity.text.match(/reserve table/ig)){
-                await dc.begin('reserveTable');
-            }
-        }
+            await dc.continue(); // Continue the current dialog if one is currently active
 
-        if(!context.responded){
-            // Continue executing the "current" dialog, if any.
-            var status = await dc.continue();
-
-            if(!context.responded && isMessage){
-                // Default message
-                await context.sendActivity("Hi! I'm a simple bot. Please say 'Hi' or 'Reserve table'.");
+            if (!context.responded) {
+                await dc.begin('greetings');
             }
         }
     });
 });
 
-// Define dialogs
-
-// Greet user by name:
+// Greet user:
 // Ask for the user name and then greet them by name.
-dialogs.add('greeting',[
+// Ask them where they work.
+dialogs.add('greetings',[
     async function (dc){
-        dc.activeDialog.state = {};
-        await dc.prompt('textPrompt', 'Hi! What is your name?');
+        await dc.prompt('numberPrompt', 'How many people in your party?', { retryPrompt: `Sorry, please specify the number of people in your party.` })
     },
-    async function(dc, results){
-        dc.activeDialog.state.userName = results;
-        await dc.context.sendActivity(`Hi ${dc.activeDialog.state.userName}!`);
-        await dc.prompt('textPrompt', 'Where do you work?');
+    async function(dc, userName){
+        await dc.context.sendActivity(`${userName} people!`);
+
+        // Ask them where they work
+        const list = ['green', 'blue'];
+        await dc.prompt('choicePrompt', 'Please make a choice', list, {retryPrompt: 'Please choose a color.'});
     },
-    async function(dc, results){
-        dc.activeDialog.state.workPlace = results;
-        await dc.context.sendActivity(`${dc.activeDialog.state.workPlace} is a fun place.`);
-        return dc.end(results); // Ends the dialog
+    async function(dc, color){
+        await dc.context.sendActivity(`${color.value} is a cool place!`);
+        await dc.begin('newGreeting');
+        // await dc.end(color);
+    }, async function(dc, name){
+        await dc.context.sendActivity(`Your name is: ${name}`);
+        await dc.end(name);
     }
 ]);
 
-// Define prompts
-// Generic prompts
-dialogs.add('numberPrompt', new NumberPrompt());
+dialogs.add('newGreeting', [
+    async function(dc){
+        await dc.prompt('textPrompt', 'What is your name?')
+    }, 
+    async function(dc, name){
+        await dc.end(name);
+    }
+])
+
+dialogs.add('choicePrompt', new ChoicePrompt());
 dialogs.add('textPrompt', new TextPrompt());
-dialogs.add('dateTimePrompt', new DatetimePrompt());
-dialogs.add('partySizePrompt', new NumberPrompt());
-
-// Reserve a table:
-// Help the user to reserve a table
-
-dialogs.add('reserveTable', [
-    async function(dc, args, next){
-        await dc.context.sendActivity("Welcome to the reservation service.");
-
-        dc.activeDialog.state.reservationInfo = {}; // Clears any previous data
-        await dc.prompt('dateTimePrompt', "Please provide a reservation date and time.");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.dateTime = result[0].value;
-
-        // Ask for next info
-        await dc.prompt('partySizePrompt', "How many people are in your party?");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.partySize = result;
-
-        // Ask for next info
-        await dc.prompt('textPrompt', "Who's name will this be under?");
-    },
-    async function(dc, result){
-        dc.activeDialog.state.reservationInfo.reserveName = result;
-        
-        // Persist data
-        var convo = conversationState.get(dc.context);; // conversationState.get(dc.context);
-        convo.reservationInfo = dc.activeDialog.state.reservationInfo;
-
-        // Confirm reservation
-        var msg = `Reservation confirmed. Reservation details: 
-            <br/>Date/Time: ${dc.activeDialog.state.reservationInfo.dateTime} 
-            <br/>Party size: ${dc.activeDialog.state.reservationInfo.partySize} 
-            <br/>Reservation name: ${dc.activeDialog.state.reservationInfo.reserveName}`;
-        await dc.context.sendActivity(msg);
-        await dc.end();
-    }
-]);
+dialogs.add('numberPrompt', new NumberPrompt());
